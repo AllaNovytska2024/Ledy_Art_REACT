a6045ec979d5957a1dde46519ec0e2abea413ba1
Alla Novytska <anovytska2022u@gmail.com>
2024-09-17 16:58:34 +0200

    Ledy-deploy

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import "./index.css";
 // import MyForm from "./components/myForm/MyForm";
-// import LedyArt from "./ledyArt/LedyArt";
+import LedyArt from "./ledyArt/LedyArt";
 // import MyInput from "./components/myInput/MyInput";
 // import LoginForm from "./components/loginForm/LoginForm";
 // import HomeWork03 from "./hW/homeWork_03/HomeWork03"
 // import MyButton from "./components/myButton/MyButton";
-import Counter from "./components/counter/Count";
+// import Counter from "./components/counter/Count";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
@@ -15,12 +15,12 @@ root.render(
     {/* <h1> </h1> */}
     {/* <LoginForm/> */}
     {/* <MyInput/> */}
-    {/* <LedyArt /> */}
+    <LedyArt />
     {/* <HomeWork03/> */}
     {/* <MyForm /> */}
     {/* <MyButton/> */}
-    <Counter initial={ 100}/>
+    {/* <Counter initial={ 100}/>
     <Counter initial={ 105}/>
-    <Counter initial={ -30000}/>
+    <Counter initial={ -30000}/> */}
   </>
 );
